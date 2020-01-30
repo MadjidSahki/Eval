420bf41818c5b5f9eecdd691c363f0f42e5961ce
Abdelmadjid SAHKI <asahki@infinitesquare.com>
2020-01-30 17:04:43 +0100

    add comment

--- Eval/Program.cs
+++ Eval/Program.cs
@@ -20,6 +20,10 @@ namespace Eval
     {
         public static string Exo1(string str1)
         {
+
+            //Je créer un hashset, qui ajoute tout les char de la string dedans, puis j'initialise un stringbuilder
+            //qui va créer une string à partir de chaque char du hashset.
+            //J'utilise un string builder au lieu d'une concaténation, pour des raisons d'optimisation.
             HashSet<char> values = new HashSet<char>();
             foreach (var c in str1)
             {
@@ -39,7 +43,7 @@ namespace Eval
         {
 
             //J'ai décidé de faire une requete linQ, qui groupe tout les mêmes nombre dans une liste
-            //Puis je fais un count de chzque liste, et retourne la liste ayant pour longueur la bonne longueur
+            //Puis je fais un count de chaque liste, et retourne la liste ayant pour longueur la bonne longueur
             var count = intArray.GroupBy(e => e).Where(e => e.Count() == (intArray.Length / 2) + 1).Select(e => e.First());
 
             if (count.Count() != 0)
@@ -55,6 +59,11 @@ namespace Eval
 
         public static void Exo3(int[] intArray, int nb)
         {
+
+            //L'algo est en place, c'était l'approche qui me semblait la plus logique
+            //On  parcours le tableau, et on compare chaque nb au nb de référence, si il est inférieur on continue
+            //Sinon on repacours le tableau une 2nd fois, si le nb comparé est plus petit que le nb de référence on le laisse
+            //Sinon, on swap 
             for (int i = 0; i < intArray.Length; i++)
             {
                 if (intArray[i] >= nb)
